306293d18b8cb8ff1aef173c72850e17ddbdea5d
siambillah <siambillahf@gmail.com>
2023-08-15 05:06:37 +0600

    rkdm project commit on local repository

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* Jquery Function Start here*/
+
+$(function(){
+    $("#header").load("/assets/myheader.html");
+})
+$(function(){
+    $("#footer").load("/assets/footer.html");
+    
+})
+$(function(){
+    $("#sidebar").load("/assets/sidebar.html");
+})
+
+/* Jquery Function end*/
+
+
